2100bc0209a841a0cbebb02707e411ff78016db3
James Sammut <juma2791@gmail.com>
2024-03-15 19:42:09 +1100

    Random sleeper

--- vector-clocks.py
+++ vector-clocks.py
@@ -1,6 +1,7 @@
 from mpi4py import MPI
 from datetime import datetime
-
+from random import randint
+from time import sleep
 # MPI World Setup (Lafayette 2021) (Dalcin 2020).
 comm = MPI.COMM_WORLD
 iproc = comm.Get_rank()
@@ -10,6 +11,13 @@ def main():
     if iproc == 0:
         print("Root process @ {0}".format(str(datetime.now().strftime("%H:%M:%S.%f"))))
     else:
+        sleeper = randint(1, 5)
+        print("Process {0} decided at {1} to sleep for {2} seconds".format(
+            iproc, 
+            datetime.now().strftime("%H:%M:%S.%f"), 
+            sleeper
+        ))
+        sleep(sleeper)
         print("Process {0} @ {1}".format(iproc, datetime.now().strftime("%H:%M:%S.%f")))
 
 main()
