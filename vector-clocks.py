3e5ecbf3e9ebfe6697da4518bf362f59222bcd64
James Sammut <juma2791@gmail.com>
2024-03-17 15:34:21 +1100

    Matrix nxn array initial, ith index incrementing [i][0]

--- vector-clocks.py
+++ vector-clocks.py
@@ -1,15 +1,24 @@
 from mpi4py import MPI
+import numpy
 from datetime import datetime
 import random
 from time import sleep
+
 # MPI World Setup (Lafayette 2021) (Dalcin 2020).
 comm = MPI.COMM_WORLD
 iproc = comm.Get_rank()
 nproc = comm.Get_size()
 
 def main():
+    vector_arr = numpy.zeros((nproc, nproc))
     if iproc == 0:
+        vector_arr[iproc][0] = 1
+        print(vector_arr)
         print("Root process @ {0}".format(str(datetime.now().strftime("%H:%M:%S.%f"))))
+        for i in range(1, nproc):
+            vector_arr_recv = comm.recv(source=i, tag=0)
+            print("Process 0 received V.A from process {0}".format(i))
+            print(vector_arr_recv)
     else:
         sleeper = random.uniform(0, 10)
         print("Process {0} decided at {1} to sleep for {2} seconds".format(
@@ -18,7 +27,10 @@ def main():
             sleeper
         ))
         sleep(sleeper)
-        print("Process {0} @ {1}".format(iproc, datetime.now().strftime("%H:%M:%S.%f")))
+        # Increment vector array at index 0
+        vector_arr[iproc][0] = 1
+        print("Process {0} sending @ {1}".format(iproc, datetime.now().strftime("%H:%M:%S.%f")))
+        comm.send(vector_arr, dest=0, tag=0)
 
 main()
 
@@ -26,4 +38,5 @@ main()
 Refs
 https://stackoverflow.com/questions/7588511/format-a-datetime-into-a-string-with-milliseconds 13th March
 https://stackoverflow.com/questions/6088077/how-to-get-a-random-number-between-a-float-range 15th March
+https://numpy.org/doc/stable/reference/generated/numpy.zeros.html 16th March
 '''
